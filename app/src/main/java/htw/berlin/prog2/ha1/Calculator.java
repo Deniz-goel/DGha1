d165d1d892770917be15a331d66de51fef2603d1
Deniz <s0571123@htw-berlin.de>
2025-04-20 19:54:03 +0200

    5. Commit Fix

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -1,4 +1,5 @@
 package htw.berlin.prog2.ha1;
+
 /**
  * Eine Klasse, die das Verhalten des Online Taschenrechners imitiert, welcher auf
  * https://www.online-calculator.com/ aufgerufen werden kann (ohne die Memory-Funktionen)
